4ab4582866496dcbac7565aafc42a9c4ab9fdeac
CrazyPrince <86961470+CrazyPrince@users.noreply.github.com>
2024-07-17 08:07:37 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,22 +6,22 @@ function convertToBool(text, fault = 'true') {
 
 
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
-const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
-const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
-global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
+const BOTNAME = 'CRAZY-MD|-WA-BOT™';
+const FOOTERNAME = '> CRAZY -  ＭＤ - Ｖ1';
+global.owner = process.env.OWNER_NUMBER  || '237620114013' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || 'youre session id' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
-OWNER_NAME: process.env.OWNER_NAME || 'VAJIRA' ,
-COMMAND_TYPE: process.env.COMMAND_TYPE || 'button' ,
-LOGO: process.env.LOGO || `https://telegra.ph/file/ab0ea6ad90e2eb98090d9.jpg` ,
+OWNER_NAME: process.env.OWNER_NAME || 'CRAZY-MF' ,
+COMMAND_TYPE: process.env.COMMAND_TYPE || 'list' ,
+LOGO: process.env.LOGO || `https://telegra.ph/file/e1c97c509ab9dc093bcca.jpg` ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,
-FOOTER: process.env.FOOTER || '‌👨‍💻 ＶＡＪＩＲＡ -  ＭＤ - Ｖ5 👨‍💻',    
-PREFIX: process.env.PREFIX || '.' ,
-LANG:  process.env.LANG || 'SI' ,
+FOOTER: process.env.FOOTER || '‌👨‍💻 CRAZY -  ＭＤ - Ｖ1 👨‍💻',
+PREFIX: process.env.PREFIX || '#' ,
+LANG:  process.env.LANG || 'FR' ,
 ANTI_BAD: process.env.ANTI_BAD || false  ,
 AUTO_REACT:  process.env.AUTO_REACT  || false  ,    
 AUTO_TYPING:  process.env.AUTO_TYPING  || false  ,
@@ -44,6 +44,6 @@ AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,
 AUTO_VOICE:  process.env.AUTO_VOICE  || false  ,
 AUTO_STICKER: process.env.AUTO_STICKER || false  ,
 WELCOME:  process.env.WELCOME  || false ,
-ANTI_DELETE : process.env.ANTI_DELETE || false ,
-DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO    
+ANTI_DELETE : process.env.ANTI_DELETE || true ,
+DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '237620114013' : process.env.DELETEMSGSENDTO    
 };
